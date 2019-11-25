1e9884fe873f49cf47d6d02b286f828a2bf6fbc7
Sam Woodman <swoodman213@gmail.com>
2019-11-25 14:59:00 -0800

    Added inst with format pdf, vignette displaying pdf, function to save pdf where desired by user

---
+++ R/das_format_pdf.R
@@ -0,0 +1,30 @@
+#' DAS format requirements
+#'
+#' Save the pdf document describing the DAS format required by \code{swfscDAS} to a specified file
+#'
+#' @param file character vector, the name of the file where the pdf will be saved
+#' @param ... passed on to \code{\link[base:files]{file.copy}}, might included named argument \code{overwrite}
+#'
+#' @details A wrapper function for \code{\link[base:files]{file.copy}}. This function
+#'   saves the pdf document describing the DAS data format requirements by
+#'   copying the pdf document located at \code{system.file("DAS_Format.pdf", package = "swfscDAS")}
+#'   to \code{file}
+#'
+#' @return \code{TRUE} if writing of file was successful, and \code{FALSE} otherwise
+#'
+#' @examples
+#' \dontrun{
+#' das_format_pdf("DAS_Format.pdf", overwrite = FALSE)
+#' }
+#'
+#' @export
+das_format_pdf <- function(file, ...) {
+  stopifnot(
+    inherits(file, "character"),
+    length(file) == 1
+  )
+
+  file.copy(
+    system.file("DAS_Format.pdf", package = "swfscDAS"), to = file, ...
+  )
+}
