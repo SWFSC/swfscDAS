6d7b2190ebadfe16a42d90a68161e667d1e18fb6
Sam Woodman <swoodman213@gmail.com>
2019-12-31 16:12:01 -0800

    Created das sample file

---
+++ data-raw/das_sample.R
@@ -0,0 +1,147 @@
+### code to prepare `das_sample` dataset goes here
+# Read in real DAS file and 'jigger' data for sample file
+
+###############################################################################
+library(dplyr)
+library(gdata)
+library(lubridate)
+library(stringr)
+library(swfscDAS)
+
+
+###############################################################################
+# # Browse through ALLDAS for section with most event types
+# x.all <- das_read("../DAS_files/AllDas.das")
+# x.all <- x.all[-753324, ]
+#
+# window.size <- 200
+# window.start <- seq(1, nrow(x.all) - window.size, by = 50)
+#
+# # system.time({ #~15s for window size of 300
+# tmp <- list()
+# for (i in window.start) {
+#   x.curr <- x.all[i:(i + window.size), ]
+#   tmp <- c(tmp, list(sort(unique(x.curr$Event))))
+#   rm(x.curr)
+# }; rm(i)
+# # })
+#
+# table(sapply(tmp, length))
+# tmp.good <- which(sapply(tmp, length) >= 24)
+# x.sel <- lapply(window.start[tmp.good], function(i) {
+#   x.all[i:(i + window.size), ]
+# })
+# table(x.all$Event)
+# lapply(x.sel, function(j) table(j$Event))
+
+
+###############################################################################
+#------------------------------------------------------------------------------
+# # Get 'original' data that will be jiggered
+# x.orig <- x.sel[[4]][29:193, ]
+# saveRDS(x.orig, file = "data-raw/x_orig.rds")
+x.orig <- readRDS("data-raw/x_orig.rds")
+
+# rm(list = ls()[!(ls() %in% "x.orig")])
+source("data-raw/das_sample_funcs.R")
+
+#------------------------------------------------------------------------------
+x <- head(x.orig, 1) %>%
+  # Add initial B line
+  mutate(Event = "B", event_num = event_num - 1,
+         Data1 = "1611", Data2 = "C", Data3 = "7", Data4 = "Y") %>%
+  bind_rows(x.orig) %>%
+  # Adjust dates and lat/lons
+  mutate(DateTime = DateTime + days(round(runif(1, min = 10, max = 20) * 365, 0)),
+         Lat = Lat + runif(1, min = 20, max = 30),
+         Lon = Lon - runif(1, min = 10, max = 20)) %>%
+  # 'Reset' file
+  mutate(file_das = "das_sample.das")
+
+
+#------------------------------------------------------------------------------
+# 'Randomize' data reported in DAS file
+# TODO: Change:
+#   DONE Cruise number in B events
+#   DONE Specific comments
+#   Sightings: Sight no (S, K, A), species, groupsize
+#     Make sure resighting info matches
+#   Turtle sighings: species, number
+#   DONE Add F event?
+#   DONE Remove Q events
+
+# Items not changed: TO REVIEW WITH JIM
+#   P events, as well as sighting observer codes
+#   V and W events
+#   N events, but Data (esp Data1, ship course) is going to not match with 'random' positions..
+
+
+# Add F event, remove Q events, and set cruise number
+stopifnot(x$Event[136] == "N")
+x <- x %>%
+  add_row(Event = "F", EffortDot = FALSE, Lat = x[136, "Lat"], Lon = x[136, "Lon"],
+          DateTime = x[136, "DateTime"],
+          Data1 = "126", Data2 = "000", Data3 = "2.7", Data4 = "0.5",
+          file_das = "das_sample.das",
+          .after = 136) %>%
+  filter(Event != "Q") %>%
+  mutate(Data1 = ifelse(Event == "B", 1000, Data1))
+
+
+# Adjust species code and groupsizes; make on sighitng multi-species
+event.A <- which(x$Event == "A")
+event.1 <- which(x$Event == "1")
+event.num <- which(x$Event %in% 1:7)
+
+x$Data5[event.A] <- c("075", "018", "079", "079", "096")
+x$Data6[event.A] <- c(NA, "016", NA, NA, NA)
+x$Data5[which(x$Event == "?")] <- "074"
+
+x$Data2[event.num] <- as.character(as.numeric(x$Data2[event.num]) + rpois(length(event.num), 1.5))
+x$Data3[event.num] <- as.character(as.numeric(x$Data2[event.num]) + rpois(length(event.num), 1))
+x$Data4[event.num] <- as.character(as.numeric(x$Data2[event.num]) - rpois(length(event.num), 1))
+x$Data4[event.num] <- ifelse(as.numeric(x$Data4[event.num]) < 1, 1, x$Data4[event.num])
+
+x[event.1[2], paste0("Data", 1:6)] <- c("001", "20", "30", "10", "60", "40")
+
+# Remove initials from comment
+stopifnot(sum(x$Event == "C") == 2)
+tmp <- x$Data9[which(x$Event == "C")[2]]
+x$Data9[which(x$Event == "C")[2]] <- str_sub(tmp, end = nchar(tmp) - 8)
+rm(tmp)
+
+
+
+#------------------------------------------------------------------------------
+# Final step: set event numbers, starting at 1 for each B, ...
+event.num.which <- which(!(x$Event %in% c(1:9, "?")))
+event1 <- sum(event.num.which < which(x$Event == "B")[2])
+event.num <- c(seq_len(event1), 1:(length(event.num.which) - event1))
+stopifnot(length(event.num) == length(event.num.which))
+x$event_num[!(x$Event %in% c(1:9, "?"))] <- event.num
+rm(event.num, event.num.which, event1)
+
+# ... and set line numbers, post inserting/deleting rows
+x$line_num <- seq_len(nrow(x))
+
+
+#------------------------------------------------------------------------------
+# Write DAS data to file
+raw_das_fwf(x, file = "inst/das_sample.das", 47)
+
+
+# usethis::use_data("das_sample")
+
+
+###############################################################################
+# # Testing
+# x <- das_read("../DAS_files/STAR1630.das")
+# raw_das_fwf(x, file = "data-raw/STAR1630_test.das", 130)
+# x2 <- das_read("data-raw/STAR1630_test.das")
+#
+# all.equal(x, x2)
+# # Data column differences are due to original files not having extra spaces for
+# #   comment rows after the text ends
+
+
+###############################################################################
---
+++ data-raw/das_sample_funcs.R
@@ -0,0 +1,109 @@
+# Functions used in data-raw/das_sample
+
+#------------------------------------------------------------------------------
+chr_z <- function(i, j = 2) sprintf(paste0("%0", j, "d"), i)
+
+
+#------------------------------------------------------------------------------
+str_pad_data <- function(i, j1 = 5) {
+  ifelse(toupper(x$Event) == "C", i, str_pad(i, width = j1, side = "left"))
+}
+
+
+#------------------------------------------------------------------------------
+# Adapted from sp::dd2dms
+dd2dms_df <- function (dd, NS = FALSE) {
+  sdd <- sign(dd)
+  # WS <- ifelse(sdd < 0, TRUE, FALSE)
+  dd <- abs(dd)
+  deg <- as(floor(dd), "integer")
+  dd <- (dd - deg) * 60
+  mins <- as(floor(dd), "integer")
+  sec <- (dd - mins) * 60
+  tst <- abs(sec - 60) > sqrt(.Machine$double.eps)
+  sec <- ifelse(tst, sec, 0)
+  mins <- ifelse(tst, mins, mins + 1)
+  tst <- mins < 60
+  mins <- ifelse(tst, mins, 0)
+  deg <- ifelse(tst, deg, deg + 1)
+  # dms <- new("DMS", WS = WS, deg = deg, min = mins, sec = sec,
+  #            NS = NS)
+  # tst <- validObject(dms)
+  # if (is.logical(tst) & tst)
+  #   return(dms)
+  # else stop(tst)
+  # dms
+
+  deg.char <- dplyr::case_when(
+    sdd < 0 & NS ~ "S",
+    sdd > 0 & NS ~ "N",
+    sdd > 0 & !NS ~ "E",
+    sdd < 0 & !NS ~ "W",
+  )
+
+  data.frame(
+    deg_char = deg.char, deg = deg, min = mins, sec = sec,
+    stringsAsFactors = FALSE
+  )
+}
+
+
+#------------------------------------------------------------------------------
+raw_das_fwf <- function(x, file, data9len = 100) {
+  ### Inputs:
+  # x: data.frame; output of das_read()
+  # file: character; file path to whcih to write fixed-width file
+  # data9len: numeric; width of 'data8' data in output text file;
+  #   final element of 'width' argument of gdata::write.fwf
+
+  ### Ouput: Writes fwf to path specified by 'file'
+
+  if (data9len < 5) warning("data9len < 5")
+
+  ### Process output of das_read
+  na.paste <- c("NA", "NANA", "NANANA")
+  x.proc <- x %>%
+    mutate(EffortDot = ifelse(EffortDot, ".", " "),
+           tm_hms = paste0(chr_z(hour(DateTime)), chr_z(minute(DateTime)),
+                           chr_z(second(DateTime))),
+           da_mdy = paste0(chr_z(month(DateTime)), chr_z(day(DateTime)),
+                           substr(chr_z(year(DateTime)), 3, 4)),
+           tm_hms = ifelse(tm_hms %in% na.paste, "", tm_hms),
+           da_mdy = ifelse(da_mdy %in% na.paste, "", da_mdy))
+
+  x.lat <- dd2dms_df(x$Lat, NS = TRUE)
+  x.lon <- dd2dms_df(x$Lon, NS = FALSE)
+
+  x.df <- data.frame(
+    x.proc$event_num, x.proc$Event, x.proc$EffortDot,
+    x.proc$tm_hms, " ", x.proc$da_mdy, " ",
+    Lat1 = x.lat$deg_char, Lat2 = chr_z(x.lat$deg, 2), ":",
+    Lat3 = sprintf("%05.2f", x.lat$min + x.lat$sec/60), " ",
+    Lon1 = x.lon$deg_char, Lon2 = chr_z(x.lon$deg, 3), ":",
+    Lon3 = sprintf("%05.2f", x.lon$min + x.lon$sec/60),
+    str_pad_data(x$Data1), str_pad_data(x$Data2), str_pad_data(x$Data3),
+    str_pad_data(x$Data4), str_pad_data(x$Data5), str_pad_data(x$Data6),
+    str_pad_data(x$Data7), str_pad_data(x$Data8), str_pad_data(x$Data9),
+    stringsAsFactors = FALSE
+  )
+  # For debugging, as necessary
+  names(x.df) <- c(
+    "event_num", "Event", "EffortDot", "tm_hms", "blank1", "da_mdy", "blank2",
+    "Lat1", "Lat2", "c1", "Lat3", "blank3", "Lon1", "Lon2", "c1", "Lon3",
+    paste0("Data", 1:9)
+  )
+  which.nona <- which(names(x.df) %in% c("Event", paste0("Data", 1:9)))
+  x.df[is.na(x.df$event_num), -which.nona] <- NA
+
+  fwf.width <- c(3, 1, 1, 6, 1, 6, 1,
+                 1, 2, 1, 5, 1,
+                 1, 3, 1, 5,
+                 rep(5, 8), data9len)
+  stopifnot(length(fwf.width) == ncol(x.df))
+
+  ### Write to fwf
+  gdata::write.fwf(
+    x.df, file = file, na = "", sep = "", colnames = FALSE,
+    justify = "left", width = fwf.width
+  )
+}
