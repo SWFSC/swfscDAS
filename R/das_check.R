a1fe7ab9d4a52de05e1f537ac586c0f90a7e7327
Sam Woodman <swoodman213@gmail.com>
2020-01-27 16:53:25 -0800

    Added some sighting checks to das_check

--- R/das_check.R
+++ R/das_check.R
@@ -14,11 +14,26 @@
 #'   \item Event codes are one of the following: #, *, ?, 1, 2, 3, 4, 5, 6, 7, 8,
 #'     A, B, C, E, F, k, K, N, P, Q, R, s, S, t, V, W, g, p, X, Y, Z.
 #'   \item The effort dot matches effort determined using B, R, and E events
-#'   \item
+#'   \item And event/column pairs meet the following requirements:
 #' }
-#' \tabular{lrr}{
-#'   \emph{Item}  \tab \emph{Columns} \tab \emph{Format}\cr
-#'   Event number \tab 1-3\cr
+#'
+#' \tabular{llll}{
+#'   \emph{Item} \tab \emph{Event} \tab \emph{Column} \tab \emph{Requirement}\cr
+#'   Cruise number  \tab B \tab Data1 \tab Can be converted to a numeric value\cr
+#'   Mode           \tab B \tab Data2 \tab Must be one of C, P, c, p, or NA (blank)\cr
+#'   Effort type    \tab R \tab Data1 \tab Must be one of F, N, S, or NA (blank)\cr
+#'   Course         \tab N \tab Data1 \tab Can be converted to a numeric value\cr
+#'   Beaufort       \tab V \tab Data1 \tab Can be converted to a numeric value\cr
+#'   Swell height   \tab V \tab Data2 \tab Can be converted to a numeric value\cr
+#'   Rain or fog    \tab W \tab Data1 \tab Can be converted to a numeric value\cr
+#'   Horizontal sun \tab W \tab Data2 \tab Can be converted to a numeric value\cr
+#'   Vertical sun   \tab W \tab Data3 \tab Can be converted to a numeric value\cr
+#'   Visibility     \tab W \tab Data5 \tab Can be converted to a numeric value\cr
+#'   NA \tab S, K, M \tab Data3-7    \tab Can be converted to a numeric value \cr
+#'   NA \tab s, k    \tab Data2-5    \tab Can be converted to a numeric value \cr
+#'   NA \tab t       \tab Data3-5, 7 \tab Can be converted to a numeric value \cr
+#'   NA \tab F       \tab Data2-4    \tab Can be converted to a numeric value \cr
+#'   NA \tab 1-8     \tab Data2-8    \tab Can be converted to a numeric value \cr
 #' }
 #'
 #' @return
@@ -36,13 +51,13 @@
 das_check <- function(file, file.out = NULL) {
   error.out <- data.frame(LineNum = NA, ID = NA, Description = NA)
   x <- das_read(file)
-  x.proc <- das_process(x)
-  x.all <- left_join(
-    x, x.proc,
-    by = c("Event", "DateTime", "Lat", "Lon", "Data1", "Data2", "Data3",
-           "Data4", "Data5", "Data6", "Data7", "Data8", "Data9",
-           "EventNum", "file_das", "line_num")
-  )
+  # x.proc <- das_process(x)
+  # x.all <- left_join(
+  #   x, x.proc,
+  #   by = c("Event", "DateTime", "Lat", "Lon", "Data1", "Data2", "Data3",
+  #          "Data4", "Data5", "Data6", "Data7", "Data8", "Data9",
+  #          "EventNum", "file_das", "line_num")
+  # )
   x.lines <- substr(readLines(file), 4, 39)
   stopifnot(nrow(x) == length(x.lines))
 
@@ -62,16 +77,37 @@ das_check <- function(file, file.out = NULL) {
 
   #----------------------------------------------------------------------------
   ### Check that effort dot matches effort determined by B/R to E events
-  err.eff.which <- which(
-    x.all$OnEffort != x.all$EffortDot & !(x.all$Event %in% c("?", 1:8))
-  )
-  #^ will be of length 0 if none, so nothing will be added to error.out
+  ndx.B <- which(x$Event == "B")
+  ndx.R <- which(x$Event == "R")
+  ndx.E <- which(x$Event == "E")
 
-  error.out <- rbind(
-    error.out,
-    list(err.eff.which, x.lines[err.eff.which],
-         rep("Effort dot does not match B/R to E effort", length(err.eff.which)))
-  )
+  if (length(ndx.E) != length(ndx.R)) {
+    # TODO
+    # stop("Error: There are not an equal number of 'R' and 'E' events",
+    #      "in the provided DAS file")
+
+  } else if (!all(ndx.E - ndx.R > 0) | !all(head(ndx.E, -1) < ndx.R[-1])) {
+    # TODO
+    # stop("Error: Not all 'R' events are followed by 'E' events")
+
+  } else {
+    x.eff.idx <- unlist(mapply(function(i, j) {
+      i:(j-1)
+    }, ndx.R, ndx.E, SIMPLIFY = FALSE))
+    ndx.B.preR <- ndx.B[(ndx.B + 1) %in% ndx.R]
+    x.eff <- seq_len(nrow(x)) %in% c(x.eff.idx, ndx.B.preR)
+
+    err.eff.which <- which(
+      x.eff != x$EffortDot & !(x$Event %in% c("?", 1:8, "#"))
+    )
+    #^ will be of length 0 if none, so nothing will be added to error.out
+
+    error.out <- rbind(
+      error.out,
+      list(err.eff.which, x.lines[err.eff.which],
+           rep("Effort dot does not match B/R to E effort", length(err.eff.which)))
+    )
+  }
 
 
   #----------------------------------------------------------------------------
@@ -83,65 +119,102 @@ das_check <- function(file, file.out = NULL) {
   #   and "txt".event code'.'data# column' for the txt to go in error.out
 
   # Cruise number
-  z.b.1 <- .check_numeric(x.proc, "B", "Data1")
+  idx.b.1 <- .check_numeric(x, "B", "Data1")
   txt.b.1 <- "Cruise number (Data1 of B events) cannot be converted to a numeric"
 
   # Mode
-  z.b.2 <- .check_character(x.proc, "B", "Data2", c("C", "P", "c", "p", NA))
+  idx.b.2 <- .check_character(x, "B", "Data2", c("C", "P", "c", "p", NA))
   txt.b.2 <- "Effort type (Data2 of B events) is not one of C, P, c, p, or NA"
 
   # Effort type
-  z.r.1 <- .check_character(x.proc, "R", "Data1", c("F", "N", "S", NA))
+  idx.r.1 <- .check_character(x, "R", "Data1", c("F", "N", "S", NA))
   txt.r.1 <- "Effort type (Data1 of R events) is not one of F, N, S, or NA"
 
   # Course
-  z.n.1 <- .check_numeric(x.proc, "N", "Data1")
+  idx.n.1 <- .check_numeric(x, "N", "Data1")
   txt.n.1 <- "Course (Data1 of N events) cannot be converted to a numeric"
 
   # Beaufort
-  z.v.1 <- .check_numeric(x.proc, "V", "Data1")
+  idx.v.1 <- .check_numeric(x, "V", "Data1")
   txt.v.1 <- "Beaufort (Data1 of V events) cannot be converted to a numeric"
 
   # Swell Height
-  z.v.2 <- .check_numeric(x.proc, "V", "Data2")
+  idx.v.2 <- .check_numeric(x, "V", "Data2")
   txt.v.2 <- "Swell height (Data2 of V events) cannot be converted to a numeric"
 
   # RainFog
-  z.w.1 <- .check_numeric(x.proc, "W", "Data1")
+  idx.w.1 <- .check_numeric(x, "W", "Data1")
   txt.w.1 <- "Rain/fog (Data1 of W events) cannot be converted to a numeric"
 
   # Horizontal sun
-  z.w.2 <- .check_numeric(x.proc, "W", "Data2")
+  idx.w.2 <- .check_numeric(x, "W", "Data2")
   txt.w.2 <- "Horizontal sun (Data2 of W events) cannot be converted to a numeric"
 
   # Vertical sun
-  z.w.3 <- .check_numeric(x.proc, "W", "Data3")
+  idx.w.3 <- .check_numeric(x, "W", "Data3")
   txt.w.3 <- "Vertical sun (Data3 of W events) cannot be converted to a numeric"
 
   # Visibility
-  z.w.5 <- .check_numeric(x.proc, "W", "Data5")
+  idx.w.5 <- .check_numeric(x, "W", "Data5")
   txt.w.5 <- "Visibility (Data5 of W events) cannot be converted to a numeric"
 
 
   # Add text to error.out as needed and return
   error.out <- rbind(
     error.out,
-    list(z.b.1, x.lines[z.b.1], rep(txt.b.1, length(z.b.1))),
-    list(z.b.2, x.lines[z.b.2], rep(txt.b.2, length(z.b.2))),
-    list(z.r.1, x.lines[z.r.1], rep(txt.r.1, length(z.r.1))),
-    list(z.n.1, x.lines[z.n.1], rep(txt.n.1, length(z.n.1))),
-    list(z.v.1, x.lines[z.v.1], rep(txt.v.1, length(z.v.1))),
-    list(z.v.2, x.lines[z.v.2], rep(txt.v.2, length(z.v.2))),
-    list(z.w.1, x.lines[z.w.1], rep(txt.w.1, length(z.w.1))),
-    list(z.w.2, x.lines[z.w.2], rep(txt.w.2, length(z.w.2))),
-    list(z.w.3, x.lines[z.w.3], rep(txt.w.3, length(z.w.3))),
-    list(z.w.5, x.lines[z.w.5], rep(txt.w.5, length(z.w.5)))
+    .check_list(x, x.lines, idx.b.1, txt.b.1),
+    .check_list(x, x.lines, idx.b.2, txt.b.2),
+    .check_list(x, x.lines, idx.r.1, txt.r.1),
+    .check_list(x, x.lines, idx.n.1, txt.n.1),
+    .check_list(x, x.lines, idx.v.1, txt.v.1),
+    .check_list(x, x.lines, idx.v.2, txt.v.2),
+    .check_list(x, x.lines, idx.w.1, txt.w.1),
+    .check_list(x, x.lines, idx.w.2, txt.w.2),
+    .check_list(x, x.lines, idx.w.3, txt.w.3),
+    .check_list(x, x.lines, idx.w.5, txt.w.5)
   )
 
 
   #----------------------------------------------------------------------------
-  # Check Data# columns for sightings
-  # TODO
+  # Check Data# columns for sightings data format
+  idx.skm.num <- .check_numeric_sight(x, c("S", "K", "M"), paste0("Data", 3:7)) #3:9
+  txt.skm.num <- paste(
+    "At least one of the Data3-9 columns for S, K, and M events",
+    "cannot be converted to a numeric"
+  )
+
+  idx.res.num <- .check_numeric_sight(x, c("s", "k"), paste0("Data", 2:5))
+  txt.res.num <- paste(
+    "At least one of the Data2-5 columns for s and k events",
+    "cannot be converted to a numeric"
+  )
+
+  idx.t.num <- .check_numeric_sight(x, "t", paste0("Data", c(3:5, 7)))
+  txt.t.num <- paste(
+    "At least one of the Data3-5/Data7 columns for t events",
+    "cannot be converted to a numeric"
+  )
+
+  idx.f.num <- .check_numeric_sight(x, "F", paste0("Data", 2:4))
+  txt.f.num <- paste(
+    "At least one of the Data2-4 columns for F events",
+    "cannot be converted to a numeric"
+  )
+
+  idx.num.num <- .check_numeric_sight(x, 1:8, paste0("Data", 2:8))
+  txt.num.num <- paste(
+    "At least one of the Data2-8 columns for 1-8 events",
+    "cannot be converted to a numeric"
+  )
+
+  error.out <- rbind(
+    error.out,
+    .check_list(x, x.lines, idx.skm.num, txt.skm.num),
+    .check_list(x, x.lines, idx.res.num, txt.res.num),
+    .check_list(x, x.lines, idx.t.num, txt.t.num),
+    .check_list(x, x.lines, idx.f.num, txt.f.num),
+    .check_list(x, x.lines, idx.num.num, txt.num.num)
+  )
 
 
   #----------------------------------------------------------------------------
--- R/swfscDAS-internal.R
+++ R/swfscDAS-internal.R
@@ -32,17 +32,20 @@
   ### Output: indices of z that cannot be converted to a numeric
 
   stopifnot(
-    inherits(z, "das_df"),
+    inherits(z, "das_dfr"),
     z.col %in% paste0("Data", 1:9)
   )
+
+  z$idx <- seq_len(nrow(z))
 
   z.curr <- z[z$Event == event.code, ]
   z.vec <- z.curr[[z.col]]
 
-  z1.na <- which(is.na(z.vec))
-  z2.na <- which(is.na(suppressWarnings(as.numeric(z.vec))))
+  z1.na <- is.na(z.vec)
+  z2.na <- is.na(suppressWarnings(as.numeric(z.vec)))
+  stopifnot(all(which(z1.na) %in% which(z2.na)))
 
-  z$line_num[z2.na[!(z2.na %in% z1.na)]]
+  z.curr$idx[z2.na != z1.na]
 }
 
 
@@ -54,16 +57,58 @@
   ### Output: indices of z where z.col is not one of vals.accepted
 
   stopifnot(
-    inherits(z, "das_df"),
+    inherits(z, "das_dfr"),
     z.col %in% paste0("Data", 1:9)
   )
+
+  z$idx <- seq_len(nrow(z))
 
   z.curr <- z[z$Event == event.code, ]
   z.vec <- z.curr[[z.col]]
 
-  z.curr$line_num[!(z.vec %in% vals.accepted)]
+  z.curr$idx[!(z.vec %in% vals.accepted)]
 }
 
 
+.check_numeric_sight <- function(z, event.code, z.col) {
+  # z: das_df object
+  # event.code: character; event code by which to filter z
+  # z.col: Column which to check; must be one of the Data# columns
+  ### Output: indices of z that cannot be converted to a numeric
+
+  stopifnot(
+    inherits(z, "das_dfr"),
+    z.col %in% paste0("Data", 1:9)
+  )
+
+  z$idx <- seq_len(nrow(z))
+  z.out <- c()
+
+  for (i in event.code) {
+    for (j in z.col) {
+      z.curr <- z[z$Event == i, ]
+      z.vec <- z.curr[[j]]
+
+      z1.na <- is.na(z.vec)
+      z2.na <- is.na(suppressWarnings(as.numeric(z.vec)))
+      stopifnot(all(which(z1.na) %in% which(z2.na)))
+
+      z.out <- c(z.out, z.curr$idx[z2.na != z1.na])
+    }
+  }
+
+  sort(unique(z.out))
+}
+
+
+.check_list <- function(z1, z2, z3, z4) {
+  # z1: x.proc
+  # z2: x.lines
+  # z3: idx.
+  # z4: txt.
+  ### Output: list formatted to be added to error.out
+
+  list(z1$line_num[z3], z2[z3], rep(z4, length(z3)))
+}
 
 ###############################################################################
